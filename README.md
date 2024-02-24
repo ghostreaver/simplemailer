2498bd67b1d952da8123dbd819597f98bd23f5f4
NeosLab <github@neoslab.com>
2024-02-24 01:34:19 +0100

    Update project repository

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# simplemailer
-Simple mailer wriiten in Python
+# SimpleMailer
+
+SimpleMailer is a tool written in Python and using `smtplib` allowing to send email on behalf of Postfix.
---
+++ simplemailer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# Import libraries
+import smtplib
+
+
+# Display prompt in the console
+def prompt(val):
+    return input(val).strip()
+
+
+# Retrieve the mail parameters
+mailfrom = prompt("From: ")
+maildest = prompt("Dest: ").split()
+subject = prompt("Subject: ").split()
+print("Enter message, end with ^D (Unix) or ^Z (Windows):")
+
+# Add the 'From' and 'To' headers at the start
+msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" % (mailfrom, ", ".join(maildest), ", ".join(subject)))
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    if not line:
+        break
+    msg = msg + line
+
+# Execute the mailer
+print("Message length is", len(msg))
+server = smtplib.SMTP('localhost')
+server.set_debuglevel(1)
+server.sendmail(mailfrom, maildest, msg)
+server.quit()
